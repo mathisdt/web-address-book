1d9137694447ba6a1c725c1351334b415ff2cd63
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2010-04-02 21:14:58 +0000

    added HypersonicSQL which now starts automatically when the application's context is started in its container (e.g. Tomcat)
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/web-address-book/trunk@265 f37aa4ac-6963-46a2-a1ff-558a307fa857

---
+++ web-address-book/src/java/org/zephyrsoft/wab/DatabaseManagerContextListener.java
@@ -0,0 +1,38 @@
+package org.zephyrsoft.wab;
+
+import javax.servlet.*;
+import org.hsqldb.Server;
+
+public class DatabaseManagerContextListener implements ServletContextListener {
+	
+	private static ServletContextListener _instance = null;
+	
+	private Server server = null;
+	
+	public DatabaseManagerContextListener() {
+		if (_instance == null) {
+			_instance = this;
+		} else {
+			throw new IllegalStateException("another instance of this class already exists");
+		}
+	}
+	
+	public void contextInitialized(ServletContextEvent sce) {
+		server = new Server();
+        server.setDatabaseName(0, "test");
+        server.setDatabasePath(0, "jdbc:hsqldb:file:db/db");
+        server.setLogWriter(null);
+        server.setErrWriter(null);
+        server.start();
+        System.out.println("started hsqldb server");
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		if (server!=null) {
+			server.stop();
+			System.out.println("stopped hsqldb server");
+		} else {
+			System.out.println("couldn't stop hsqldb server - reference was null");
+		}
+	}
+}
