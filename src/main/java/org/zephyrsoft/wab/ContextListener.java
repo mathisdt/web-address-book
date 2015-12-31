334ac2039f3a450913974ec82c1f6a9581abb4fa
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2015-12-31 21:31:48 +0000

    use LOG4J 2
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/web-address-book/trunk@670 f37aa4ac-6963-46a2-a1ff-558a307fa857

--- src/main/java/org/zephyrsoft/wab/ContextListener.java
+++ src/main/java/org/zephyrsoft/wab/ContextListener.java
@@ -1,12 +1,19 @@
 package org.zephyrsoft.wab;
 
-import java.io.*;
-import java.sql.*;
-import java.util.*;
-import javax.servlet.*;
-import com.avaje.ebean.*;
-import org.apache.log4j.*;
-import org.h2.tools.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.EbeanServerFactory;
 
 /**
  * central class for initializing the environment (init logging, load properties, init persistence)
@@ -15,6 +22,7 @@ import org.h2.tools.*;
  */
 public class ContextListener implements ServletContextListener {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(ContextListener.class);
 	private static final Object LOCK = new Object();
 	
 	private static ContextListener instance = null;
@@ -23,23 +31,15 @@ public class ContextListener implements ServletContextListener {
 		synchronized (LOCK) {
 			if (instance != null) {
 				IllegalStateException exception = new IllegalStateException(Constants.ANOTHER_INSTANCE_EXISTS);
-				Log.error(Constants.ANOTHER_INSTANCE_EXISTS, exception);
+				LOG.error(Constants.ANOTHER_INSTANCE_EXISTS, exception);
 				throw exception;
 			}
 			instance = this;
 		}
-		// get properties for log4j and environment
-		Properties log4jProps = new Properties();
+		// get properties for environment
 		Properties environmentProps = new Properties();
-		InputStream log4jStream = null;
 		InputStream environmentStream = null;
 		try {
-			log4jStream = getClass().getResourceAsStream(Constants.LOG4J_PROPERTIES);
-			if (log4jStream != null) {
-				log4jProps.load(log4jStream);
-			} else {
-				throw new IllegalStateException(Constants.PROPERTIES_NOT_FOUND);
-			}
 			environmentStream = getClass().getResourceAsStream(Constants.ENVIRONMENT_PROPERTIES);
 			if (environmentStream != null) {
 				environmentProps.load(environmentStream);
@@ -49,13 +49,6 @@ public class ContextListener implements ServletContextListener {
 		} catch (IOException e) {
 			throw new IllegalStateException(Constants.PROPERTIES_NOT_FOUND, e);
 		} finally {
-			try {
-				if (log4jStream != null) {
-					log4jStream.close();
-				}
-			} catch (IOException e) {
-				// ignore at this point
-			}
 			try {
 				if (environmentStream != null) {
 					environmentStream.close();
@@ -69,8 +62,6 @@ public class ContextListener implements ServletContextListener {
 		if (prop1 != null && prop1.trim().equalsIgnoreCase(Boolean.TRUE.toString())) {
 			useDedicatedDatabase = true;
 		}
-		// initialize logging system
-		PropertyConfigurator.configure(log4jProps);
 		
 		// get database name
 		String databaseName = null;
@@ -104,6 +95,7 @@ public class ContextListener implements ServletContextListener {
 	private boolean useDedicatedDatabase = false;
 	private Server server = null;
 	
+	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		// start H2 database is configured
 		if (useDedicatedDatabase) {
@@ -111,26 +103,27 @@ public class ContextListener implements ServletContextListener {
 				server = Server.createTcpServer(); // use argument "-tcpAllowOthers" if all interfaces should be bound
 													// (and not only "localhost")
 				server.start();
-				Log.info(Constants.STARTED_H2_SERVER);
+				LOG.info(Constants.STARTED_H2_SERVER);
 			} catch (SQLException e) {
-				Log.info(Constants.COULD_NOT_START_H2_SERVER);
+				LOG.info(Constants.COULD_NOT_START_H2_SERVER);
 				e.printStackTrace();
 			}
 		} else {
-			Log.info(Constants.DID_NOT_START_H2_SERVER);
+			LOG.info(Constants.DID_NOT_START_H2_SERVER);
 		}
 	}
 	
+	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		if (useDedicatedDatabase) {
 			if (server != null) {
 				server.stop();
-				Log.info(Constants.STOPPED_H2_SERVER);
+				LOG.info(Constants.STOPPED_H2_SERVER);
 			} else {
-				Log.info(Constants.COULD_NOT_STOP_H2_SERVER);
+				LOG.info(Constants.COULD_NOT_STOP_H2_SERVER);
 			}
 		} else {
-			Log.info(Constants.DID_NOT_STOP_H2_SERVER);
+			LOG.info(Constants.DID_NOT_STOP_H2_SERVER);
 		}
 	}
 }
