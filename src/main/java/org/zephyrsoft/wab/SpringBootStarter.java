69eadb730c361f4384b54584c68991d94ee99188
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2017-03-01 21:41:41 +0100

    make JDBC parameters configurable externally

--- src/main/java/org/zephyrsoft/wab/SpringBootStarter.java
+++ src/main/java/org/zephyrsoft/wab/SpringBootStarter.java
@@ -9,13 +9,23 @@ import org.zephyrsoft.wab.util.ApplicationContextProvider;
 
 import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
+import com.avaje.ebean.config.DataSourceConfig;
+import com.avaje.ebean.config.ServerConfig;
 
 @SpringBootApplication
 @ServletComponentScan
 public class SpringBootStarter {
 	
-	@Value("${wab.databaseName}")
-	private String databaseName;
+	@Value("${wab.db.username}")
+	private String databaseUsername;
+	@Value("${wab.db.password}")
+	private String databasePassword;
+	@Value("${wab.db.url}")
+	private String databaseUrl;
+	@Value("${wab.db.driver}")
+	private String databaseDriver;
+	@Value("${wab.db.heartbeat}")
+	private String databaseHeartbeat;
 	
 	@Bean
 	public ApplicationContextProvider ApplicationContextProvider() {
@@ -24,7 +34,22 @@ public class SpringBootStarter {
 	
 	@Bean
 	public EbeanServer ebeanServer() {
-		return EbeanServerFactory.create(databaseName);
+		DataSourceConfig dsConfig = new DataSourceConfig();
+		dsConfig.setUsername(databaseUsername);
+		dsConfig.setPassword(databasePassword);
+		dsConfig.setUrl(databaseUrl);
+		dsConfig.setDriver(databaseDriver);
+		dsConfig.setHeartbeatSql(databaseHeartbeat == null || databaseHeartbeat.trim().isEmpty()
+			? null
+			: databaseHeartbeat);
+		dsConfig.setMinConnections(1);
+		dsConfig.setMaxConnections(20);
+		
+		ServerConfig config = new ServerConfig();
+		config.setDataSourceConfig(dsConfig);
+		config.setName("default");
+		
+		return EbeanServerFactory.create(config);
 	}
 	
 	public static void main(final String[] args) {
