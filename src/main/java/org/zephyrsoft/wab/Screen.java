ee6eca46216818e905ef78a6a2a6fa006ce7eba3
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2017-03-02 22:04:38 +0100

    remove superfluous things

--- src/main/java/org/zephyrsoft/wab/Screen.java
+++ src/main/java/org/zephyrsoft/wab/Screen.java
@@ -3,6 +3,11 @@ package org.zephyrsoft.wab;
 import java.util.Collections;
 import java.util.List;
 
+import org.zephyrsoft.wab.model.Family;
+import org.zephyrsoft.wab.util.DataUtil;
+import org.zephyrsoft.wab.util.EchoElementStore;
+import org.zephyrsoft.wab.util.EchoUtil;
+
 import nextapp.echo.app.Border;
 import nextapp.echo.app.Border.Side;
 import nextapp.echo.app.Button;
@@ -19,28 +24,23 @@ import nextapp.echo.app.event.ActionListener;
 import nextapp.echo.filetransfer.app.DownloadCommand;
 import nextapp.echo.filetransfer.app.DownloadProvider;
 
-import org.zephyrsoft.wab.model.Family;
-import org.zephyrsoft.wab.util.DataUtil;
-import org.zephyrsoft.wab.util.EchoElementStore;
-import org.zephyrsoft.wab.util.EchoUtil;
-
 /**
  * container for the whole application's UI
  * 
  * @author Mathis Dirksen-Thedens
  */
 public class Screen extends ContentPane {
-
+	
 	private static final long serialVersionUID = 3483818110434112144L;
-
+	
 	private EchoElementStore elements = null;
-
+	
 	private AddressBookApp app = null;
-
+	
 	private Button downloadPdf = null;
 	private Button addFamily = null;
 	private Column data = null;
-
+	
 	public Screen(AddressBookApp app) {
 		super();
 		this.app = app;
@@ -55,7 +55,7 @@ public class Screen extends ContentPane {
 		downloadPdf = EchoUtil.createButton("Download as PDF", null, Constants.BUTTON_REPORT);
 		downloadPdf.addActionListener(new ActionListener() {
 			private static final long serialVersionUID = 9132114868976616925L;
-
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				downloadPdf();
@@ -64,7 +64,7 @@ public class Screen extends ContentPane {
 		addFamily = EchoUtil.createButton("add family", null, Constants.BUTTON_ADD_FAMILY);
 		addFamily.addActionListener(new ActionListener() {
 			private static final long serialVersionUID = 9132114868976616923L;
-
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -86,7 +86,7 @@ public class Screen extends ContentPane {
 		Row buttonRow = new Row();
 		buttonRow.add(downloadPdf);
 		topColumn.add(buttonRow);
-
+		
 		data = new Column();
 		Side[] border = new Side[] { new Side(new Extent(0), Color.BLACK, Border.STYLE_NONE),
 			new Side(new Extent(0), Color.BLACK, Border.STYLE_NONE),
@@ -94,21 +94,21 @@ public class Screen extends ContentPane {
 			new Side(new Extent(0), Color.BLACK, Border.STYLE_NONE) };
 		data.setBorder(new Border(border));
 		topColumn.add(data);
-
+		
 		List<Family> families = DataUtil.createQuery(Family.class).findList();
 		Collections.sort(families);
-
+		
 		for (Family f : families) {
 			FamilyPanel panel = new FamilyPanel(f, elements);
 			data.add(panel);
 		}
-
+		
 		Row bottomRow = new Row();
 		bottomRow.setCellSpacing(new Extent(10));
 		bottomRow.add(addFamily);
 		topColumn.add(bottomRow);
 	}
-
+	
 	private void initReusableElements() {
 		elements = new EchoElementStore();
 		elements.put(Constants.BUTTON_UP_GREEN, EchoUtil.createImage(Constants.BUTTON_UP_GREEN));
@@ -119,12 +119,11 @@ public class Screen extends ContentPane {
 		elements.put(Constants.BUTTON_ADD_PERSON, EchoUtil.createImage(Constants.BUTTON_ADD_PERSON));
 		elements.put(Constants.BUTTON_DELETE_PERSON, EchoUtil.createImage(Constants.BUTTON_DELETE_PERSON));
 	}
-
+	
 	private void downloadPdf() {
-		// TODO move to controller
 		DownloadProvider provider = new PdfProvider();
 		app.enqueueCommand(new DownloadCommand(provider));
-
+		
 	}
-
+	
 }
