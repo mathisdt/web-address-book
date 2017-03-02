de8f5e1ab91e51eb6433210b3a657dcf9b86517f
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2017-03-02 21:05:14 +0100

    move ebean configuration from ebean.properties to Spring context and reconfigure ebean logging

--- src/main/java/org/zephyrsoft/wab/SpringBootStarter.java
+++ src/main/java/org/zephyrsoft/wab/SpringBootStarter.java
@@ -9,6 +9,9 @@ import org.zephyrsoft.wab.util.ApplicationContextProvider;
 
 import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
+import com.avaje.ebean.LogLevel;
+import com.avaje.ebean.config.AutofetchConfig;
+import com.avaje.ebean.config.AutofetchMode;
 import com.avaje.ebean.config.DataSourceConfig;
 import com.avaje.ebean.config.ServerConfig;
 
@@ -48,6 +51,23 @@ public class SpringBootStarter {
 		ServerConfig config = new ServerConfig();
 		config.setDataSourceConfig(dsConfig);
 		config.setName("default");
+		
+		config.setDdlGenerate(false);
+		config.setDdlRun(false);
+		config.setDebugSql(false);
+		config.setDebugLazyLoad(false);
+		
+		AutofetchConfig autofetchConfig = new AutofetchConfig();
+		autofetchConfig.setQueryTuning(true);
+		autofetchConfig.setProfiling(true);
+		autofetchConfig.setMode(AutofetchMode.DEFAULT_ON);
+		autofetchConfig.setProfilingMin(1);
+		autofetchConfig.setProfilingBase(10);
+		autofetchConfig.setProfilingRate(0.05);
+		config.setAutofetchConfig(autofetchConfig);
+		
+		config.setLoggingLevel(LogLevel.NONE);
+		config.setLoggingToJavaLogger(true);
 		
 		return EbeanServerFactory.create(config);
 	}
